c09c2169f847da51a1652398d1805f23674705eb
nyramana <nyramana@student.42antananarivo.mg>
2026-08-03 14:12:59 +0300

    feat: Starting the constrained decoding

--- src/my_llm.py
+++ src/my_llm.py
@@ -1,28 +1,33 @@
 # *************************************************************************** #
 #                                                                             #
 #                                                        :::      ::::::::    #
-#    __main__.py                                       :+:      :+:    :+:    #
+#    my_llm.py                                         :+:      :+:    :+:    #
 #                                                    +:+ +:+         +:+      #
 #    By: nyramana <nyramana@student.42antananariv  +#+  +:+       +#+         #
 #                                                +#+#+#+#+#+   +#+            #
-#    Created: 2026/08/03 13:16:51 by nyramana         #+#    #+#              #
-#    Updated: 2026/08/03 14:09:52 by nyramana        ###   ########.fr        #
+#    Created: 2026/08/03 14:08:13 by nyramana         #+#    #+#              #
+#    Updated: 2026/08/03 14:11:18 by nyramana        ###   ########.fr        #
 #                                                                             #
 # *************************************************************************** #
 
+from llm_sdk import Small_LLM_Model
+
 from .parser import Parser
 
-from .my_llm import My_LLM
 
-class Main:
+class My_LLM:
     def __init__(self) -> None:
+        self.model = Small_LLM_Model()
         self.parser = Parser()
-        self.my_llm = My_LLM()
+
+        self.prompts = self.parser.load_prompts()
+        self.function_defs = self.parser.load_function_definitions()
+
+    def get_func_name(self) -> None:
+        ...
+
+    def get_func_args(self) -> None:
+        ...
 
     def run(self) -> None:
-        self.parser.check_args()
-
-
-if __name__ == "__main__":
-    main = Main()
-    main.run()
+        ...
