1e378a384f34bca1e4eb4608538d3d87a174bfaf
nyramana <nyramana@student.42antananarivo.mg>
2026-08-14 15:14:39 +0300

    feat: Starting the UI design

--- src/ui/home.py
+++ src/ui/home.py
@@ -6,16 +6,18 @@
 #    By: nyramana <nyramana@student.42antananariv  +#+  +:+       +#+         #
 #                                                +#+#+#+#+#+   +#+            #
 #    Created: 2026/08/13 16:35:44 by nyramana         #+#    #+#              #
-#    Updated: 2026/08/13 16:54:22 by nyramana        ###   ########.fr        #
+#    Updated: 2026/08/14 15:14:04 by nyramana        ###   ########.fr        #
 #                                                                             #
 # *************************************************************************** #
 
-from blessed import Terminal
+import sys
+
+from readchar import readkey
+from rich.console import Console
 
 
 class Home:
-    def __init__(self) -> None:
-        self.term = Terminal()
+    def __init__(self, models: list[str]) -> None:
         self.title = """
   ░██████             ░██ ░██    ░███     ░███               ░███     ░███                       ░██                   
  ░██   ░██            ░██ ░██    ░████   ░████               ░████   ░████                       ░██                   
@@ -26,21 +28,37 @@ class Home:
   ░██████   ░█████░██ ░██ ░██    ░██       ░██  ░███████     ░██       ░██  ░█████░██  ░█████░██ ░██░█████   ░███████  
                                                                                              ░██                       
                                                                                        ░███████                        
-                                                                                                                       
         """
+        self.models = models
 
-    def run(self) -> None:
-        with self.term.fullscreen(), self.term.cbreak(), self.term.hidden_cursor():
-            print(self.term.clear)
-            print(self.title)
-            while True:
-
-                key = self.term.inkey(timeout=1)
-
-                if key.lower() == "q":
-                    break
+    def get_model_name(self) -> str:
+        console = Console()
+        console.clear()
+        console.print(f"{self.title}", style="blue")
+        console.print("Choose your llm:\n", style="bold")
+        for index, model in enumerate(self.models, start=1):
+            console.print(f"    {index}: [yellow]{model}[/yellow]")
+        while True:
+            console.print()
+            value = readkey()
+            if value.lower() == "q":
+                console.print("Exiting...", style="blue")
+                sys.exit()
+            if not value.isnumeric():
+                console.print("[red]Error: Invalid input[/red]")
+                continue
+            value = int(value)
+            if len(self.models) >= value:
+                console.print(f"[green]Choosing {self.models[value - 1]}...[/green]")
+                break
+            else:
+                console.print("[red bold]Error: Invalid input[/red bold]")
+        return self.models[value - 1]
 
 
 if __name__ == "__main__":
-    home = Home()
-    home.run()
+    try:
+        home = Home(["Qwen/Qwen3-0.6B", "HuggingFaceTB/SmolLM2-360M-Instruct"])
+        home.get_model_name()
+    except KeyboardInterrupt:
+        ...
